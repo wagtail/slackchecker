6b9ab651345f298c07461af0ca7c9de81296b066
Matt Westcott <matt@west.co.tt>
2023-02-15 14:46:57 +0000

    test failed successfully

--- .github/slackchecker.py
+++ .github/slackchecker.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-r = requests.head("https://wagtail.org/badslack", headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"}, allow_redirects=True)
+r = requests.head("https://wagtail.org/slack", headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"}, allow_redirects=True)
 if r.url.startswith('https://wagtailcms.slack.com/join/shared_invite/'):
     print("Slack invitation link is OK")
 else:
